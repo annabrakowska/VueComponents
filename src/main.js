f2c6f2d7b4b7b8a08b8ec6ab0ed2b7087e5a969c
Anna <anna.brakowska91@gmail.com>
2019-11-10 00:17:04 -0500

    updated main.js to import components and styles

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import components from './components';
+import './styles/main.scss';
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+export default {
+  install(Vue) {
+    Vue.mixin({ components });
+  }
+};
